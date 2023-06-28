c452c5c417ad63a4077748a2c9fc55ee97b03b2d
olayinka2023 <olayinka9@gmail.com>
2023-06-28 20:14:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var tablinks = document.getElementsByClassName('tab-links');
+    var tabcontents = document.getElementsByClassName('tab-contents');
+
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classlist.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classlist.remove("active-tab");
+        }
+        event.currentTarget.classlist.add("active-link");
+        document.getElementById(tabname);
+    }
+   
+    alert("Welcome to YCODING")
+console.log("Hello yinka")
